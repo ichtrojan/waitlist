a81403c2e221b3b615d43cab472a9e8b40bc2df7
Trojan <ordrizzy@gmail.com>
2024-12-10 20:25:35 +0100

    undo

--- api/waitlist.ts
+++ api/waitlist.ts
@@ -5,22 +5,8 @@ const notion = new Client({auth: process.env.NOTION_API_KEY});
 const databaseId = process.env.NOTION_DATABASE_ID as string;
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        res.status(204).end();
-        return;
-    }
-
     if (req.method !== 'POST') {
         res.status(405).json({message: 'Only POST requests are allowed'});
-        return;
-    }
-
-    if (!req.body || typeof req.body !== 'object') {
-        res.status(400).json({ message: 'invalid or missing request body'});
         return;
     }
 
